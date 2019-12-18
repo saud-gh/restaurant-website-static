c2b70853e1ac3f6b793b3d760e0572485ba2d934
devdevil-production <saudd2gh@gmail.com>
2019-12-18 16:02:43 +0400

    Add dinner, drinks and breakfast menu items

--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,115 @@
-const lunch = [
-  {
-    name: "Tikka Chicken",
-    price: 15,
-    image: "./images/grid-img4.png",
-    description:
-      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
-  },
-  {
-    name: "Tikka Chicken",
-    price: 12,
-    image: "./images/grid-img5.png",
-    description:
-      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
-  },
-  {
-    name: "Tikka Chicken",
-    price: 76,
-    image: "./images/grid-img3.png",
-    description:
-      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
-  },
-  {
-    name: "Tikka Chicken",
-    price: 34,
-    image: "./images/grid-img2.png",
-    description:
-      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
-  },
-  {
-    name: "Tikka Chicken",
-    price: 92,
-    image: "./images/grid-img1.png",
-    description:
-      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
-  }
-];
+const menu = {
+  lunch: [
+    {
+      name: "Tikka Chicken",
+      price: 15,
+      image: "./images/grid-img4.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 12,
+      image: "./images/grid-img5.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 76,
+      image: "./images/grid-img3.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 34,
+      image: "./images/grid-img2.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 92,
+      image: "./images/grid-img1.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    }
+  ],
+  breakfast: [
+    {
+      name: "Tikka Chicken",
+      price: 15,
+      image: "./images/grid-img4.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 12,
+      image: "./images/grid-img5.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 76,
+      image: "./images/grid-img3.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 92,
+      image: "./images/grid-img1.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    }
+  ],
+  dinner: [
+    {
+      name: "Tikka Chicken",
+      price: 34,
+      image: "./images/grid-img2.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 92,
+      image: "./images/grid-img1.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    }
+  ],
+  drinks: [
+    {
+      name: "Tikka Chicken",
+      price: 15,
+      image: "./images/grid-img4.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 12,
+      image: "./images/grid-img5.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 76,
+      image: "./images/grid-img3.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    },
+    {
+      name: "Tikka Chicken",
+      price: 92,
+      image: "./images/grid-img1.png",
+      description:
+        "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+    }
+  ]
+};
