c45a5c69093cdb6f0ffce32abd32493669ee15df
devdevil-production <saudd2gh@gmail.com>
2019-12-18 13:05:14 +0400

    Add new item

--- js/menu.js
+++ js/menu.js
@@ -2,28 +2,35 @@ const lunch = [
   {
     name: "Tikka Chicken",
     price: 15,
+    image: "./images/grid-img4.png",
+    description:
+      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+  },
+  {
+    name: "Tikka Chicken",
+    price: 12,
+    image: "./images/grid-img5.png",
+    description:
+      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+  },
+  {
+    name: "Tikka Chicken",
+    price: 76,
     image: "./images/grid-img3.png",
     description:
       "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
   },
   {
     name: "Tikka Chicken",
-    price: 15,
-    image: "./images/grid-img3.png",
+    price: 34,
+    image: "./images/grid-img2.png",
     description:
       "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
   },
   {
     name: "Tikka Chicken",
-    price: 15,
-    image: "./images/grid-img3.png",
-    description:
-      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
-  },
-  {
-    name: "Tikka Chicken",
-    price: 15,
-    image: "./images/grid-img3.png",
+    price: 92,
+    image: "./images/grid-img1.png",
     description:
       "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
   }
