0e172eac1eb41b0cad557fcaecac1d8663da8be7
devdevil-production <saudd2gh@gmail.com>
2019-12-18 12:15:18 +0400

    Create menu js file + Add lunch menu items

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+const lunch = [
+  {
+    name: "Tikka Chicken",
+    price: 15,
+    image: "./images/grid-img3.png",
+    description:
+      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+  },
+  {
+    name: "Tikka Chicken",
+    price: 15,
+    image: "./images/grid-img3.png",
+    description:
+      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+  },
+  {
+    name: "Tikka Chicken",
+    price: 15,
+    image: "./images/grid-img3.png",
+    description:
+      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+  },
+  {
+    name: "Tikka Chicken",
+    price: 15,
+    image: "./images/grid-img3.png",
+    description:
+      "Lorem ipsum dolor sit amet, risus sed nonummy at, sit volutpat."
+  }
+];
